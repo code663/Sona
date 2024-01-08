02bcdd8d03a769427712fb179e4d699f499b395a
MRPAWANMUSIC <146386254+code663@users.noreply.github.com>
2024-01-08 23:15:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/WCGKING/BRANDED-NEW-MUSIC",
+    "https://github.com/code663/sona",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
